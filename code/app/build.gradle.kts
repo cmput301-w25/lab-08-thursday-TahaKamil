48faea70a8eab9172c7043644111cef3015dc023
“Taha <tkamil@ualberta.ca>
2025-03-07 08:56:04 -0700

    Fixes and changes

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -38,10 +38,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("net.bytebuddy:byte-buddy:1.17.1")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.7.0")
     testImplementation("org.mockito:mockito-inline:5.2.0")
+    implementation("net.bytebuddy:byte-buddy:1.17.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
